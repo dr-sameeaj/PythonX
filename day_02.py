a4b69061a8632cb38a84200ae15a37d7b8321904
dr-sameeaj <sambieber3331@gmail.com>
2026-01-02 11:47:19 +0530

    Day 02 - mastering variables and data types for ML foundations

---
+++ day_02.py
@@ -0,0 +1,15 @@
+# Variables representing ML Model Parameters
+developer_name = "Sameeraj"
+model_name = "Neural Network v1"
+learning_rate = 0.01
+is_trained = False
+epochs = 50
+accuracy_score = 0.89
+
+# Using built-in functions on data-like structures
+print(type(learning_rate))  # Check if it's a float
+print(len(model_name))      # Length of the string
+
+# we can print a few things as follows
+
+print(developer_name + " is developing a model named " + model_name + ". It is his first time trying this.")
